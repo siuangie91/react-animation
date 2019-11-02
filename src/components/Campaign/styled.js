169da1421c49592200756cff4cd8b171470f6248
siuangie91 <siuangie91@gmail.com>
2019-11-02 18:07:45 -0400

    change background color

--- src/components/Campaign/styled.js
+++ src/components/Campaign/styled.js
@@ -6,14 +6,10 @@ export const campaign = css`
   height: 350px;
   border-bottom: 1px solid #333;
   box-shadow: 0 0 8px #333;
-  /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#bf5454+0,ffffff+80 */
   background: #bf5454; /* Old browsers */
   background: -moz-linear-gradient(top,  #bf5454 0%, #ffffff 80%); /* FF3.6-15 */
   background: -webkit-linear-gradient(top,  #bf5454 0%,#ffffff 80%); /* Chrome10-25,Safari5.1-6 */
   background: linear-gradient(to bottom,  #bf5454 0%,#ffffff 80%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
   filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#bf5454', endColorstr='#ffffff',GradientType=0 ); /* IE6-9 */
-  
-  
-
   z-index: 2;
 `;
